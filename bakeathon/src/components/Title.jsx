6b79d40d0e6f26caa9f60c504b1889fc198b22f6
Tanner Creque <tanner.creque@gmail.com>
2021-04-13 15:48:35 -0600

    Title changes

--- bakeathon/src/components/Title.jsx
+++ bakeathon/src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Title = () => {
     return (
         <div>
-           <h1 className='title'>Dream Puffs</h1> 
+           <h1 className='title'>Marc's Air Fryer Functional Recipe Finder</h1> 
         </div>
     )
 }
