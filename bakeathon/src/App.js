e01aca10cf4b293063e8973bd16e48f5cde56838
Tanner Creque <tanner.creque@gmail.com>
2021-04-13 11:01:11 -0600

    JesseDrives

--- bakeathon/src/App.js
+++ bakeathon/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
+import Form from './components/Form';
+import Fryer from './components/Folder/airFUCKINGfryer.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title /> 
+      <Form />
+      <img src={ Fryer } alt="Fryer"/>
     </div>
   );
 }
---
+++ bakeathon/src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+            <h1>Form</h1>
+            <form className="form">
+            <input type="text" className="ingredient-input"></input>
+            <input type="submit"></input>
+            </form>
+        </div>
+    )
+}
+
+export default Form
